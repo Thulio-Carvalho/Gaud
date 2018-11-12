fa382f3d6dfff8ce1aa6e7ed397572039237da94
ramonssarmento <ramon.sarmento@ccc.ufcg.edu.br>
2018-11-12 14:41:07 -0300

    Hotfix

--- src/Comandos.py
+++ src/Comandos.py
@@ -1,251 +1,302 @@
 #coding: utf-8
 import json
 import subprocess
-from time import sleep, time
 import re
+from time import sleep, time
+import requests
+from bs4 import BeautifulSoup
 
 
 class Comandos:
+    def _update(self):
+        arquivo = open("comandos.json", "r")
+        self.comandos = json.load(arquivo)
+        arquivo.close()
 
-	def _update(self):
-		arquivo = open("comandos.json","r")
-		self.comandos = json.load(arquivo)
-		arquivo.close()
+    def is_command(self, text):
+        self._update()
+        return any(text.startswith(x) for x in self.comandos)
 
-	def is_command(self,text):
-		self._update()
-		yield any(text.startswith(x) for x in self.comandos)
-	
+    def executar(self, comando):
+        self._update()
+        for i in self.comandos:
+            if comando.startswith(i):
+                comando = comando.lstrip(i).lstrip()
+                return eval(self.comandos[i])(comando)
+        return ["Comando não cadastrado"]
 
-	def executar(self,comando):
-		self._update()
-		for i in self.comandos:
-			if comando.startswith(i):
-				comando = comando.lstrip(i).strip()
-				yield eval(self.comandos[i])(comando)
-		yield ["Comando não cadastrado"]
-	
-	def _reset(self,comando):
-		#yield ["Não está funcionando"]
-		print("Reset")
-		#print (subprocess.Popen("nohup bash runme.sh&", shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, universal_newlines=True).communicate()[0])
-		from Comandos import Comandos as aux
-		self = aux()
-		yield ["Restarting...","May not work"]
-	
-	def permitidos(self):
-		def jsonKeys2int(x):
-			if isinstance(x, dict):
-					yield {int(k):v for k,v in x.items()}
-			yield x
-		arquivo = open("permitidos.json","r")
-		permitidos = json.load(arquivo,object_hook=jsonKeys2int)
-		arquivo.close()
-		yield permitidos
-	
-	def wait(self):
-		def jsonKeys2int(x):
-			if isinstance(x, dict):
-					yield {int(k):v for k,v in x.items()}
-			yield x
-		arquivo = open("wait.json","r")
-		wait = json.load(arquivo,object_hook=jsonKeys2int)
-		arquivo.close()
-		yield wait
-	
-	def _see_problems(self,comando):
-		arquivo = open("problemas.json","r")
-		wait = json.load(arquivo)
-		arquivo.close()
-		yield sorted(wait)
+    def _reset(self, comando):
+        #return ["Não está funcionando"]
+        print("Reset")
+        #print (subprocess.Popen("nohup bash runme.sh&", shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, universal_newlines=True).communicate()[0])
+        from Comandos import Comandos as aux
+        self = aux()
+        return ["Restarting...", "May not work"]
 
-	def _see_admins(self,comando):
-		def get_value(item):
-			yield item[1].lower()
-		admins = self.permitidos()
-		yield ["%s (%i)" % (v,k) for k,v in sorted(admins.items(),key=get_value)]
+    def permitidos(self):
+        def jsonKeys2int(x):
+            if isinstance(x, dict):
+                return {int(k): v for k, v in x.items()}
+            return x
 
-	def _add_admin(self,comando):
-		comando = comando.split(" ")
-		if comando[0] != "" and len(comando) < 2 or not comando[0].isdigit():
-			yield["Formato invalido","Tente: /add_admin id nome"]
-		permitidos = self.permitidos()
-		permitidos[int(comando[0])] = " ".join(comando[1:])
-		arquivo = open("permitidos.json","w")
-		json.dump(permitidos,arquivo)
-		arquivo.close()
-		self._remove_wait(comando[0])
-		yield ["Sucesso"]
-	
-	def _remove_admin(self,comando):
-		if not comando.isdigit():
-			yield["Formato invalido","Tente: /remove_admin id"]
-		permitidos = self.permitidos()
-		if int(comando) in permitidos:
-			del permitidos[int(comando)]
-			arquivo = open("permitidos.json","w")
-			json.dump(permitidos,arquivo)
-			arquivo.close()
-			
-			yield ["Sucesso"]
-		else:
-			yield ["Admin não cadastrado"]
+        arquivo = open("permitidos.json", "r")
+        permitidos = json.load(arquivo, object_hook=jsonKeys2int)
+        arquivo.close()
+        return permitidos
 
-	def _ligar_maquina(self,comando):
-		yield ["Em Construção"]
-	
-	def _status_laboratorio(self,comando):
-		maquinas = self._get_maquinas()
-		if comando in ['lcc1','lcc2','lcc3']:
-			geral = [self._status_maquina(i)[0] for i in sorted(maquinas) if i.startswith(comando)]
-			ligadas = []
-			desligadas = []
-			defeito = 0
-			
-			for i in geral:
-				if "desligada" in i.lower():
-					desligadas.append(i)
-				elif "ligada" in i.lower():
-					ligadas.append(i)
-				elif "defeito" in i.lower():
-					desligadas.append(i)
-					ligadas.append(i)
-					defeito += 1
-                        yield (ligadas if len(ligadas) < len(desligadas) else desligadas) +  ["ligadas: %d, desligadas: %d, defeito: %d, total: %d" %(len(ligadas) - defeito,len(desligadas) - defeito,defeito,len(geral))]
-			
-				
-		else:
-			yield ["Está laboratorio não existe","tente digitar apenas o nome do laboratorio"]
+    def wait(self):
+        def jsonKeys2int(x):
+            if isinstance(x, dict):
+                return {int(k): v for k, v in x.items()}
+            return x
 
+        arquivo = open("wait.json", "r")
+        wait = json.load(arquivo, object_hook=jsonKeys2int)
+        arquivo.close()
+        return wait
 
-	def _status_maquina(self,comando):
-		maquinas = self._get_maquinas()
-		if comando in maquinas:
-			if subprocess.Popen("ping %s -c 1 -W 1 -w 1| grep '1 received'" % maquinas[comando]['ip'], shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, universal_newlines=True).communicate()[0] != "":
-				yield [comando + " Ligada"]
-			elif comando in self._see_problems(""):
-				yield [comando + " DEFEITO"]
-			else:
-				yield [comando + " Desligada"]
-		else:
-			yield ["Está maquinas não existe","tente laboratorio-maquina"]
+    def _see_problems(self, comando):
+        arquivo = open("problemas.json", "r")
+        wait = json.load(arquivo)
+        arquivo.close()
+        return sorted(wait)
 
-	def _comandos(self,comando):
-		yield sorted(self.comandos.keys())
+    def _see_admins(self, comando):
+        def get_value(item):
+            return item[1].lower()
 
-	def _get_maquinas(self):
-		arquivo = open("funcoes/maquinas.json","r")
-		maquinas = json.load(arquivo)
-		arquivo.close()
-		yield maquinas
+        admins = self.permitidos()
+        return [
+            "%s (%i)" % (v, k)
+            for k, v in sorted(admins.items(), key=get_value)
+        ]
 
-	def _see_wait(self,comando):
-		wait = self.wait()
-		retorno = []
-		for i in wait:
-			retorno.append("%s (%d): %s" % (wait[i][0],i,wait[i][1]))
-		if len(retorno) == 0:
-			retorno.append("Sem waits restantes")
-		yield retorno
-	
-	def _remove_wait(self,comando):
-		wait = self.wait()
-		if comando.isdigit() and int(comando) in wait:
-			del wait[int(comando)]
-			
-			arquivo = open("wait.json","w")
-			json.dump(wait,arquivo)
-			arquivo.close()
-			
-			yield ["Sucesso"]
-		else:
-			yield ["ID invalido ou não cadstrado","Uso: /remove_wait id"]
-	
-	def _add_problem(self,comando):
-		if not comando:
-			yield["Formato invalido","Tente: /add_problem laboratorio-numero"]
-		problemas = self._see_problems("")
-		problemas.append(comando)
-		arquivo = open("problemas.json","w")
-		json.dump(problemas,arquivo)
-		arquivo.close()
-		self._remove_wait(comando)
-		yield ["Sucesso"]
-	
-	def _remove_problem(self,comando):
-		if not comando:
-			yield["Formato invalido","Tente: /remove_problem laboratorio-numero"]
-		problemas = self._see_problems("")
-		if comando in problemas:
-			problemas.remove(comando)
-			arquivo = open("problemas.json","w")
-			json.dump(problemas,arquivo)
-			arquivo.close()
-			
-			yield ["Sucesso"]
-		else:
-			yield ["Maquina não está com problema"]
-        
-        def _watch(self,comando):
-            if comando not in ['lcc1','lcc2','lcc3']:
-                yield ["Está laboratorio não existe","tente digitar apenas o nome do laboratorio"]
-            lcc = comando
-            status = []
-            start = time()
-            while time() - start < 120000:
-                if not status:
-                    status = status_laboratorio(lcc)
-                else:
-                    atual = status_laboratorio(lcc)
-                    for i in range(len(status)):
-                        if status[i] != atual[i]:
-                            yield ["%s: %s -> %s" %(atual[i].split("-")[0],status[i].split("-")[1],atual[i].split("-")[1])]
-                    status = atual + []
-                    sleep(1)
-        
-        def _agenda(self,comando):
-			def date_decode(date):
-				#    20181108T110000Z
-				#    08/11/2018 11:00 (00)
-				#    08/11/2018 08:00 (-03)
+    def _add_admin(self, comando):
+        comando = comando.split(" ")
+        if comando[0] != "" and len(comando) < 2 or not comando[0].isdigit():
+            return ["Formato invalido", "Tente: /add_admin id nome"]
+        permitidos = self.permitidos()
+        permitidos[int(comando[0])] = " ".join(comando[1:])
+        arquivo = open("permitidos.json", "w")
+        json.dump(permitidos, arquivo)
+        arquivo.close()
+        self._remove_wait(comando)
+        return ["Sucesso"]
 
-				day = "%s/%s/%s" % (date[6:8],date[4:6],date[0:4])
-				hour = "%02d:%s" %(int(date[9:11]) - 3,date[11:13])
-				yield [day,hour]
+    def _remove_admin(self, comando):
+        if not comando.isdigit():
+            return ["Formato invalido", "Tente: /remove_admin id"]
+        permitidos = self.permitidos()
+        if int(comando) in permitidos:
+            del permitidos[int(comando)]
+            arquivo = open("permitidos.json", "w")
+            json.dump(permitidos, arquivo)
+            arquivo.close()
 
-			def get_events(site):
-				page = requests.get(site)
+            return ["Sucesso"]
+        else:
+            return ["Admin não cadastrado"]
 
-				soup = BeautifulSoup(page.content, 'html.parser')
+    def _ligar_maquina(self, comando):
+        return ["Em Construção"]
 
-				for name in soup.find_all('a', attrs={'class': 'event-link'}):
-					page = BeautifulSoup(requests.get("https://calendar.google.com/calendar/"+name['href']).content, 'html.parser')
-					event = [name.text.strip()]
-					for time in page.find_all('time'):
-						event.append(date_decode(time['datetime']))
-					yield event
+    def _status_laboratorio(self, comando):
+        maquinas = self._get_maquinas()
+        if comando in ['lcc1', 'lcc2', 'lcc3']:
+            geral = [
+                self._status_maquina(i)[0] for i in sorted(maquinas)
+                if i.startswith(comando)
+            ]
+            ligadas = []
+            desligadas = []
+            defeito = 0
 
-			comando = comando.split()
-			if len(comando) != 2 or comando[0] not in ['lcc1','lcc2','lcc3']:
-				yield ["Fromato invalido","Tente laboratorio dd/mm"]
-			site = {"lcc1":"https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_9u9fch5o55dbgrschdeduq348c@group.calendar.google.com&mode=AGENDA","lcc2":"https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_80qc5chl59nmv2268aef8hp528@group.calendar.google.com&mode=AGENDA","lcc3":"https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_noalttgqttm3c5pm94k3ttbj1k@group.calendar.google.com&mode=AGENDA"}[comando[0]]
-			data = comando[1]
-			if data == '*':
-				yield ["%s: %s (%s - %s)" % (i[1][0],i[0],i[1][1],i[2][1]) for i in get_events(site)]
-			elif re.compile("^(0[1-9]|[12][0-9]|3[01]|[1-9])/(0[1-9]|1[012]|[1-9])$").search(data) != None:
-				yield ["%s (%s - %s)" % (i[0],i[1][1],i[2][1]) for i in get_events(site) if str(i[1][0]).startswith(data)]
-			else:
-				yield ["Data invalida","Tente dd/mm"]
-            
+            for i in geral:
+                if "desligada" in i.lower():
+                    desligadas.append(i)
+                elif "ligada" in i.lower():
+                    ligadas.append(i)
+                elif "defeito" in i.lower():
+                    desligadas.append(i)
+                    ligadas.append(i)
+                    defeito += 1
+            return (ligadas
+                    if len(ligadas) < len(desligadas) else desligadas) + [
+                        "ligadas: %d, desligadas: %d, defeito: %d, total: %d" %
+                        (len(ligadas) - defeito, len(desligadas) - defeito,
+                         defeito, len(geral))
+                    ]
 
+        else:
+            return [
+                "Está laboratorio não existe",
+                "tente digitar apenas o nome do laboratorio"
+            ]
 
+    def _status_maquina(self, comando):
+        maquinas = self._get_maquinas()
+        if comando in maquinas:
+            if subprocess.Popen(
+                    "ping %s -c 1 -W 1 -w 1| grep '1 received'" %
+                    maquinas[comando]['ip'],
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                    universal_newlines=True).communicate()[0] != "":
+                return [comando + " Ligada"]
+            elif comando in self._see_problems(""):
+                return [comando + " DEFEITO"]
+            else:
+                return [comando + " Desligada"]
+        else:
+            return ["Está maquinas não existe", "tente laboratorio-maquina"]
 
+    def _comandos(self, comando):
+        return sorted(self.comandos.keys())
 
+    def _get_maquinas(self):
+        arquivo = open("funcoes/maquinas.json", "r")
+        maquinas = json.load(arquivo)
+        arquivo.close()
+        return maquinas
 
-        
+    def _see_wait(self, comando):
+        wait = self.wait()
+        retorno = []
+        for i in wait:
+            retorno.append("%s (%d): %s" % (wait[i][0], i, wait[i][1]))
+        if len(retorno) == 0:
+            retorno.append("Sem waits restantes")
+        return retorno
 
+    def _remove_wait(self, comando):
+        wait = self.wait()
+        if comando.isdigit() and int(comando) in wait:
+            del wait[int(comando)]
 
+            arquivo = open("wait.json", "w")
+            json.dump(wait, arquivo)
+            arquivo.close()
 
+            return ["Sucesso"]
+        else:
+            return ["ID invalido ou não cadstrado", "Uso: /remove_wait id"]
 
+    def _add_problem(self, comando):
+        if not comando:
+            return [
+                "Formato invalido", "Tente: /add_problem laboratorio-numero"
+            ]
+        problemas = self._see_problems("")
+        problemas.append(comando)
+        arquivo = open("problemas.json", "w")
+        json.dump(problemas, arquivo)
+        arquivo.close()
+        self._remove_wait(comando)
+        return ["Sucesso"]
 
+    def _remove_problem(self, comando):
+        if not comando:
+            return [
+                "Formato invalido", "Tente: /remove_problem laboratorio-numero"
+            ]
+        problemas = self._see_problems("")
+        if comando in problemas:
+            problemas.remove(comando)
+            arquivo = open("problemas.json", "w")
+            json.dump(problemas, arquivo)
+            arquivo.close()
 
+            return ["Sucesso"]
+        else:
+            return ["Maquina não está com problema"]
+
+    def _watch(self, comando):
+        def status_laboratorio(comando):
+            maquinas = self._get_maquinas()
+            geral = [
+                self._status_maquina(i)[0] for i in sorted(maquinas)
+                if i.startswith(comando)
+            ]
+            return geral
+
+        if comando not in ['lcc1', 'lcc2', 'lcc3']:
+            yield "Está laboratorio não existe"
+            yield "tente digitar apenas o nome do laboratorio"
+            return
+        yield "Começando, Tempo de espera: 2 min"
+        lcc = comando
+        status = []
+        start = time()
+        while time() - start < 120:
+            if status == []:
+                status = status_laboratorio(lcc)
+            else:
+                atual = status_laboratorio(lcc)
+                for i in range(len(status)):
+                    if status[i] != atual[i]:
+                        yield ("%s: %s -> %s" % (atual[i].split(" ")[0],
+                                                 status[i].split(" ")[1],
+                                                 atual[i].split(" ")[1]))
+                status = atual
+                sleep(1)
+        yield "Watch finalizado"
+
+    def _agenda(self, comando):
+        def date_decode(date):
+            #    20181108T110000Z
+            #    08/11/2018 11:00 (00)
+            #    08/11/2018 08:00 (-03)
+
+            day = "%s/%s/%s" % (date[6:8], date[4:6], date[0:4])
+            hour = "%02d:%s" % (int(date[9:11]) - 3, date[11:13])
+            return [day, hour]
+
+	def get_events(site):
+		page = requests.get(site)
+
+		soup = BeautifulSoup(page.content, 'html.parser')
+
+		for name in soup.find_all('a', attrs={'class': 'event-link'}):
+			page = BeautifulSoup(requests.get("https://calendar.google.com/calendar/"+name['href']).content, 'html.parser')
+			event = [name.text.strip()]
+			for tempo in page.find_all('time'):
+				event.append(date_decode(tempo['datetime']))
+	#        event.append("https://calendar.google.com/calendar/"+name['href'])
+			yield event
+
+        comando = comando.split()
+        if len(comando) != 2 or comando[0] not in ['lcc1', 'lcc2', 'lcc3']:
+            yield "Fromato invalido"
+            yield "Tente laboratorio dd/mm"
+            return
+        site = {
+            "lcc1":
+            "https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_9u9fch5o55dbgrschdeduq348c@group.calendar.google.com&mode=AGENDA",
+            "lcc2":
+            "https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_80qc5chl59nmv2268aef8hp528@group.calendar.google.com&mode=AGENDA",
+            "lcc3":
+            "https://calendar.google.com/calendar/htmlembed?src=computacao.ufcg.edu.br_noalttgqttm3c5pm94k3ttbj1k@group.calendar.google.com&mode=AGENDA"
+        }[comando[0]]
+        data = comando[1]
+        if data == '*':
+			cont = 0
+			for i in get_events(site):
+				yield ("%s: %s (%s - %s)" % (i[1][0], i[0], i[1][1], i[2][1]))
+				cont += 1
+			if not cont:
+				yield "Hum..., parece que não temos nada previsto"
+        elif re.compile("^(0[1-9]|[12][0-9]|3[01]|[1-9])/(0[1-9]|1[012]|[1-9])$").search(data) != None:
+			cont = 0
+			for i in get_events(site):
+
+				if i[1][0].startswith(data):
+					yield ("%s (%s - %s)" % (i[0], i[1][1], i[2][1]))
+					cont += 1
+			if not cont:
+
+				yield "Hum..., parece que não temos nada previsto"
+                   
+        else:
+            yield "Data invalida"
+            yield "Tente dd/mm"
--- src/main.py
+++ src/main.py
@@ -6,96 +6,101 @@ import telepot
 from telepot.loop import MessageLoop
 from telepot.delegate import per_chat_id, create_open, pave_event_space
 
+
 class User(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
 
-	def __init__(self, *args, **kwargs):
-		super(User, self).__init__(*args, **kwargs)
+    def _permitidos(self, userid):
+        def jsonKeys2int(x):
+            if isinstance(x, dict):
+                return {int(k): v for k, v in x.items()}
+            return x
 
-	def _permitido(self,userid):
-		def jsonKeys2int(x):
-			if isinstance(x, dict):
-					return {int(k):v for k,v in x.items()}
-			return x
-		arquivo = open("permitidos.json","r")
-		permitidos = json.load(arquivo,object_hook=jsonKeys2int)
-		arquivo.close()
-		return userid in permitidos
+        arquivo = open("permitidos.json", "r")
+        permitidos = json.load(arquivo, object_hook=jsonKeys2int)
+        arquivo.close()
+        return userid in permitidos
 
-	def open(self, initial_msg, seed):
-		
-		content_type, chat_type, chat_id = telepot.glance(initial_msg)
-		self._is_admin = chat_id in Comandos().permitidos()
-		if not self._is_admin:
-			wait = Comandos().wait()
-			if chat_id in wait:
-				self.sender.sendMessage("Hum... Acho que você ainda não está na lista")
-				self.sender.sendMessage("mas eu já anotei seu nome")
-				
-			else:
-				self.sender.sendMessage("Quem é você?")
-			return True
-		
-		
-		
-		if content_type == 'text' and Comandos().is_command(initial_msg['text']):
-			for i in Comandos().executar(initial_msg['text']):
-				self.sender.sendMessage(i)
-		else:
-			self.sender.sendMessage('Que bom te ver')
-		wait = Comandos().wait()
-		if len(wait) != 0:
-			self.sender.sendMessage('Temos %d pedidos para verificar (/see_wait)' % len(wait))
-		
-		return True  # prevent on_message() from being called on the initial message
+    def open(self, initial_msg, seed):
 
-	def on_chat_message(self, msg):
-		content_type, chat_type, chat_id = telepot.glance(msg)
-		self._is_admin = chat_id in Comandos().permitidos()
-		
-		if content_type != 'text':
-			self.sender.sendMessage('Conexão aqui ta ruim, manda em texto pls')
-			return
+        content_type, chat_type, chat_id = telepot.glance(initial_msg)
+        self._is_admin = self._permitidos(chat_id)
+        if not self._is_admin:
+            wait = Comandos().wait()
+            if chat_id in wait:
+                self.sender.sendMessage(
+                    "Hum... Acho que você ainda não está na lista")
+                self.sender.sendMessage("mas eu já anotei seu nome")
 
-		if not self._is_admin:
-			
-		
-			wait = Comandos().wait()
-			if chat_id in wait:
-				self.sender.sendMessage("Hum... Acho que você ainda não está na lista")
-				self.sender.sendMessage("mas eu já anotei seu nome")
-				return
-			
-			nome = msg['from']["first_name"] + ((" " + msg['from']['last_name'] ) if 'last_name' in msg['from'] else "")
-			
-			wait[chat_id] = [nome,msg['text']]
-			
-			arquivo = open("wait.json","w")
-			json.dump(wait,arquivo)
-			arquivo.close()
-		
-			self.sender.sendMessage("Hum... Acho que você não está na lista")
-			self.sender.sendMessage("Vou anotar seu nome aqui, depois entro em contato")
-			return
-		
-		if Comandos().is_command(msg['text']):
-			for i in Comandos().executar(msg['text']):
-				self.sender.sendMessage(i)
-		else:
-			self.sender.sendMessage("Isso era pra ser uma ordem?")
-			self.sender.sendMessage("Porque eu não entendi")
-		
+            else:
+                self.sender.sendMessage("Quem é você?")
+            return True
 
-	def on__idle(self, event):
-		self.close()
+        if content_type == 'text' and Comandos().is_command(
+                initial_msg['text']):
+            for i in Comandos().executar(initial_msg['text']):
+                self.sender.sendMessage(i)
+        else:
+            self.sender.sendMessage('Que bom te ver')
+        wait = Comandos().wait()
+        if len(wait) != 0:
+            self.sender.sendMessage(
+                'Temos %d pedidos para verificar (/see_wait)' % len(wait))
+
+        return True  # prevent on_message() from being called on the initial message
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        self._is_admin = self._permitidos(chat_id)
+
+        if content_type != 'text':
+            self.sender.sendMessage('Conexão aqui ta ruim, manda em texto pls')
+            return
+
+        if not self._is_admin:
+
+            wait = Comandos().wait()
+            if chat_id in wait:
+                self.sender.sendMessage(
+                    "Hum... Acho que você ainda não está na lista")
+                self.sender.sendMessage("mas eu já anotei seu nome")
+                return
+
+            nome = msg['from']["first_name"] + (
+                (" " + msg['from']['last_name'])
+                if 'last_name' in msg['from'] else "")
+
+            wait[chat_id] = [nome, msg['text']]
+
+            arquivo = open("wait.json", "w")
+            json.dump(wait, arquivo)
+            arquivo.close()
+
+            self.sender.sendMessage("Hum... Acho que você não está na lista")
+            self.sender.sendMessage(
+                "Vou anotar seu nome aqui, depois entro em contato")
+            return
+
+        if Comandos().is_command(msg['text']):
+            for i in Comandos().executar(msg['text']):
+                self.sender.sendMessage(i)
+        else:
+            self.sender.sendMessage("Isso era pra ser uma ordem?")
+            self.sender.sendMessage("Porque eu não entendi")
+
+    def on__idle(self, event):
+        self.close()
+
+
 #LCCControllerBot
 TOKEN = "685454659:AAE3IoeYN-dZyabaeDxIQLaxX-9jid-Pd6k"
 
 bot = telepot.DelegatorBot(TOKEN, [
-	pave_event_space()(
-		per_chat_id(), create_open, User, timeout=300),
+    pave_event_space()(per_chat_id(), create_open, User, timeout=300),
 ])
 MessageLoop(bot).run_as_thread()
 print('Listening ...')
 
 while 1:
-	time.sleep(10)
+    time.sleep(10)
